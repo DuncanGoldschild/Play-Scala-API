9f6b17a0ec3ac8cd59877fd6fd75e9f55bcae181
DuncanGold <duncang56@gmail.com>
2019-07-10 17:10:34 +0200

    comments & update fonction

--- basicthingstotestplayscala/app/controllers/HomeController.scala
+++ basicthingstotestplayscala/app/controllers/HomeController.scala
@@ -19,6 +19,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
 
 case class Food(id : Int, name : String)
 
+//ArrayBuffer qui me sert de "BDD" en attendant la transition vers Mongo ou SQLITE etc
   val foods = ArrayBuffer(
     Food (1, "Carbonara"),
     Food (2, "Bolognaise"),
@@ -26,13 +27,13 @@ case class Food(id : Int, name : String)
     Food (4, "Potatoes")
   )
 
-  implicit val foodWrites = Json.writes[Food] //pas trop compris comment ce truc marche
-  implicit val foodReads = Json.reads[Food] //pas trop compris comment ce truc marche
+  implicit val foodWrites = Json.writes[Food] //Il faut une implicit pour faire marcher l'écriture d'un JSON
+  implicit val foodReads = Json.reads[Food] //Il faut une implicit pour faire marcher la lecture d'un JSON
   /* Les valeurs implicites de READ et WRITE sont utilisées pour la serialisation/déserialisation des JSON
   pour les GET et POST
   Celles implémentées ci-dessus et ci-dessous marchent toutes les 2
 
-  implicit val userWrites = new Writes[User] {
+  implicit val foodWrites = new Writes[User] {
     def writes(user: User) = Json.obj(
       "id" -> user.id,
       "email" -> user.email,
@@ -52,12 +53,13 @@ case class Food(id : Int, name : String)
 //Ajoute un plat avec POST /foods
   def newFood = Action { implicit request =>
     val food  = Json.fromJson[Food](request.body.asJson.get).get
+    //Verification que l'objet comporte bien les attributs attendus et que l'ID est bien libre
     if (food.id != null && food.name != null && getById(food.id) == null){
     val newfood = Food(food.id,food.name)
     foods += newfood
     println(Json.toJson(newfood) + "rajouté")
     Ok(Json.toJson(newfood))}
-    else Ok("Bad request (existing food or empty name/id)")
+    else Ok("Bad request (existing id or empty name/id)")
   }
   
 //Affiche la liste des plats avec GET /foods
@@ -65,8 +67,10 @@ case class Food(id : Int, name : String)
         Ok(Json.toJson(foods))
   }
 
-//Supprime un plat
+
+//Supprime un plat avec DELETE /delete/food/"id"
   def deleteFood(id : Int) = Action {
+    //Verification que l'ID est bien attribué
     if (getById(id) != null){
         val newfood = getById(id)
         foods -= newfood
@@ -76,8 +80,22 @@ case class Food(id : Int, name : String)
     else Ok("Plat introuvable")
   }
 
+  //Met un plat existant à jour avec PUT /food/id
+  def updateFood(id : Int) =  Action { implicit request =>
+    val previousFood = getById(id)
+    val food  = Json.fromJson[Food](request.body.asJson.get).get
+    //Verification que l'objet comporte bien les attributs attendus, que l'ID est bien existant, et que le nouvel ID est libre
+    if (food.id != null && food.name != null && previousFood != null && getById(food.id)==null){
+    val newfood = Food(food.id,food.name)
+    foods(foods.indexOf(previousFood)) = newfood
+    println(Json.toJson(newfood) + "updated")
+    Ok(Json.toJson(newfood))}
+    else Ok("Bad request (existing id or empty name/id or non existing element)")
+  }
+
 //Affiche un plat en particulier avec GET /food/"id"
   def getFood(id: Int) = 
+    //Verification que l'ID est bien attribué
       if (getById(id) == null) {
         Action {
           Ok("Incorrect ID")
@@ -88,10 +106,14 @@ case class Food(id : Int, name : String)
           Ok(Json.toJson(getById(id)))
         }
 
+//La page de base
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 
+/**
+ *Checks if a food has this ID and returns the Food object
+ */
   def getById(id:Int) = {
     val elements = foods.filter(x => (x.id == id))
     if (elements.isEmpty) null else elements(0) 
@@ -102,6 +124,6 @@ case class Food(id : Int, name : String)
 Commande linux pour POST et DELETE
 
 curl     --header "Content-type: application/json"     --request POST     --data '{"id":98, "name":"Salade"}'     http://localhost:9000/foods
-curl     --header "Content-type: application/json"     --request DELETE    http://localhost:9000/delete/food/98
-
+curl     --header "Content-type: application/json"     --request DELETE    http://localhost:9000/food/2
+curl -X PUT -H "Content-Type: application/json" -d '{"id": 10 , "name" : "spaghetti"}' "http://localhost:9000/food/2"
 */
